0fa54a50f0a37c9668249e297608d02b3bdb88c0
cathsuchurch <cathsuchurch@gmail.com>
2024-06-12 22:31:16 +0000

    'change'

--- src/app/home/TourBox/TourBox.tsx
+++ src/app/home/TourBox/TourBox.tsx
@@ -12,7 +12,7 @@ const TourBox = () => {
         <article>
           <h3>Tour The World And Experience</h3>
           <h3>Unforgettable Journeys</h3>
-          <span>To kickstart your digital adventures with (Name)</span>
+          <span>To kickstart your digital adventures with Pryme Tourism</span>
         </article>
         <Link href={''}>Check All</Link>
       </section>
